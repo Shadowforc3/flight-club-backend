e96d4712632f850ca28601f06dca6f74512bcbdc
Shadowforc3 <blade24690@hotmail.co.uk>
2026-04-13 15:53:50 +0100

    Item.js

--- models/Item.js
+++ models/Item.js
@@ -1,1 +1,11 @@
+import mongoose from "mongoose";
 
+const ItemSchema = new mongoose.Schema({
+  itemId: Number,
+  name: String,
+  category: String,
+  price: Number,
+  updated: Date
+});
+
+export default mongoose.model("Item", ItemSchema);
