6bc08c4d2d00ebe2ac9304f418e44331441e4c51
Shadowforc3 <blade24690@hotmail.co.uk>
2026-04-13 15:52:13 +0100

    server.js

--- server.js
+++ server.js
@@ -1,1 +1,23 @@
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import itemsRoute from "./routes/items.js";
+import config from "./config.js";
 
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(config.MONGO_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => console.error("MongoDB error:", err));
+
+app.use("/api/items", itemsRoute);
+
+app.get("/", (req, res) => {
+  res.send("Flight Club Backend Running");
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
