f07d3e15aefa1085be4bf6954f7fe5f9fabd5b36
Shadowforc3 <blade24690@hotmail.co.uk>
2026-04-13 15:53:16 +0100

    itemsController.js

--- controllers/itemsController.js
+++ controllers/itemsController.js
@@ -1,1 +1,10 @@
+import Item from "../models/Item.js";
 
+export async function getItems(req, res) {
+  try {
+    const items = await Item.find({});
+    res.json({ success: true, items });
+  } catch (err) {
+    res.status(500).json({ success: false, error: err.message });
+  }
+}
